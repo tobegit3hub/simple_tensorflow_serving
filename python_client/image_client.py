3c4c8b173b869654fec6c75460e9d757d5b0d92d
tobe <tobeg3oogle@gmail.com>
2018-05-10 16:42:51 +0800

    Add image client with resize image

--- python_client/image_client.py
+++ python_client/image_client.py
@@ -6,8 +6,12 @@ import requests
 def main():
   endpoint = "http://127.0.0.1:8500"
 
-  files = {"image": open("../images/mew.jpg", "rb")}
-  data = {"model_version": 1}
+  #files = {"image": open("../images/mew.jpg", "rb")}
+  #data = {"model_version": 1}
+
+  files = {"image": open("../images/mew_224_224.jpg", "rb")}
+  data = {"model_version": 1, "shape": "1,224,224,3"}
+
   response = requests.post(endpoint, files=files, data=data)
   print(response.text)
 
