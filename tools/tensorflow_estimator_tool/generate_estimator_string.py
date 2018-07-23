41d36a9e6bae2bf3eb35417f933c710bd3b1e07e
tobe <tobeg3oogle@gmail.com>
2018-07-23 17:04:25 +0800

    Add local inference script to check estimator model

--- tools/tensorflow_estimator_tool/generate_estimator_string.py
+++ tools/tensorflow_estimator_tool/generate_estimator_string.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import codecs
+import base64
 import tensorflow as tf
 
 
 def _float_feature(value):
   return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
 
 def _bytes_feature(value):
   return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
@@ -35,16 +36,22 @@ def main():
           item = float(item)
           feature_dict[keys[j]] = _float_feature(value=item)
         elif types[j] == "string":
-          feature_dict[keys[j]] = _bytes_feature(value=codecs.decode(item, "utf-8").encode("utf8"))
+          feature_dict[keys[j]] = _bytes_feature(value=item)
 
-      example = tf.train.Example(features=tf.train.Features(feature=feature_dict))
+      example = tf.train.Example(features=tf.train.Features(
+          feature=feature_dict))
       serialized = example.SerializeToString()
       serialized_strings.append(serialized)
 
-
-    serialized_proto = tf.contrib.util.make_tensor_proto(serialized_strings, dtype=tf.string)
+    serialized_proto = tf.contrib.util.make_tensor_proto(
+        serialized_strings, dtype=tf.string)
     serialized_proto_handle = serialized_proto.string_val
-    print(serialized_proto_handle)
+
+    # Example: "\n\x1f\n\x0e\n\x01a\x12\t\n\x07\n\x05hello\n\r\n\x01b\x12\x08\x12\x06\n\x04\x00\x00\x00?"
+    proto_string = serialized_proto_handle.pop()
+    base64_proto_string = base64.urlsafe_b64encode(proto_string)
+    print("Base64 string: {}".format(base64_proto_string))
+
 
 if __name__ == "__main__":
   main()
