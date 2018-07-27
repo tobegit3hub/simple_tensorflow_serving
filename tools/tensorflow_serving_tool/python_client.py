04ef355e3d0a0e33e57217ea4aaa4a259ec734d6
tobe <tobeg3oogle@gmail.com>
2018-07-27 16:40:48 +0800

    Add python client for tensorflow serving restful api

---
+++ tools/tensorflow_serving_tool/python_client.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import requests
+
+
+def main():
+
+  #endpoint = "http://127.0.0.1:8501/v1/models/default/versions/2:predict"
+  endpoint = "http://127.0.0.1:8501/v1/models/default:predict"
+
+  input_data = {
+      #"signature_name": "serving_default",
+      # "keys": [[1.0], [2.0]],
+      # "features": [[0, 0, 0, 0, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 1, 1, 1, 1]]
+      "instances": [{
+          "keys": 1,
+          "features": [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+      }, {
+          "keys": 2,
+          "features": [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+      }]
+  }
+
+  result = requests.post(endpoint, json=input_data)
+  print(result.text)
+
+
+if __name__ == "__main__":
+  main()
