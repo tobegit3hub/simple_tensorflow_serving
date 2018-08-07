6d6497fcc1d8a883bccbb9f05cd18947556e5e5e
tobe <tobeg3oogle@gmail.com>
2018-08-07 11:02:28 +0800

    Add tensorflow template application model for benchmark

--- python_client/estimator_client.py
+++ python_client/estimator_client.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import requests
-import sys
 import base64
 
 
@@ -9,18 +8,13 @@ def main():
 
   string_data1 = '\n\x1f\n\x0e\n\x01a\x12\t\n\x07\n\x05hello\n\r\n\x01b\x12\x08\x12\x06\n\x04\x00\x00\x00?'
   string_data2 = '\n \n\x0f\n\x01a\x12\n\n\x08\n\x06\xe4\xbd\xa0\xe5\xa5\xbd\n\r\n\x01b\x12\x08\x12\x06\n\x04\x00\x00\x80\xbf'
-  string_data1 = base64.urlsafe_b64encode(string_data1)
-  string_data2 = base64.urlsafe_b64encode(string_data2)
-  string_datas = [string_data1, string_data2]
+  # Example: Ch8KDgoBYRIJCgcKBWhlbGxvCg0KAWISCBIGCgQAAAA_
+  string_data1_b64 = base64.urlsafe_b64encode(string_data1)
+  string_data2_b64 = base64.urlsafe_b64encode(string_data2)
+  string_datas = [{"b64": string_data1_b64}, {"b64": string_data2_b64}]
 
   endpoint = "http://127.0.0.1:8500"
-  input_data = {
-      #"model_version": "1531881325",
-      #"model_version": "100",
-      "data": {
-          "inputs": string_datas
-      }
-  }
+  input_data = {"data": {"inputs": string_datas}}
   result = requests.post(endpoint, json=input_data)
   print(result.text)
 
--- python_client/image_client.py
+++ python_client/image_client.py
@@ -4,23 +4,11 @@ import requests
 import base64
 
 
-def base64_to_url_safe_base64(arg):
-  stripped = arg.split("=")[0]
-  filtered = stripped.replace("+", "-").replace("/", "_")
-  return filtered
-
-
-def url_safe_base64_to_base64(arg):
-  filtered = arg.replace("-", "+").replace("_", "/")
-  padded = filtered + "=" * ((len(filtered) * -1) % 4)
-  return padded
-
-
 def main():
 
   image_file_name = "../images/mew.jpg"
-  image_b64_string = base64.urlsafe_b64encode(open(image_file_name, "rb").read())
-  image_b64_strings = [image_b64_string]
+  image_b64_string = base64.urlsafe_b64encode(
+      open(image_file_name, "rb").read())
 
   endpoint = "http://127.0.0.1:8500"
   input_data = {
@@ -28,7 +16,7 @@ def main():
       #"model_version": 2,
       #"signature_name": "serving_base64",
       "data": {
-          "images": image_b64_strings
+          "images": [image_b64_string]
       }
   }
   result = requests.post(endpoint, json=input_data)
